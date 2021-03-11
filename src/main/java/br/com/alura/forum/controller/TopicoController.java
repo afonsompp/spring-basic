7106171ef76e2012d7eff06d3451589b9436e446
Afonso Mateus <afonsoprad10@gmail.com>
2021-03-11 08:55:30 -0400

    simplifying pagination and add pagination default

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -4,9 +4,8 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -39,10 +37,8 @@ public class TopicoController {
     }
 
     @GetMapping
-    public Page<TopicoDto> topicos(@RequestParam int pagina, @RequestParam int quantidade,
-            @RequestParam String ordernacao) {
-        Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.DESC, ordernacao);
-        return TopicoDto.toTopicoDto(topicoRepository.findAll(paginacao));
+    public Page<TopicoDto> topicos(@PageableDefault(sort = "id") Pageable pageable) {
+        return TopicoDto.toTopicoDto(topicoRepository.findAll(pageable));
     }
 
     @GetMapping("/{id}")
